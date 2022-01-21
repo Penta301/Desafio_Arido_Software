45b064cb7633ec24e3b4faea39a8a55841f9e9c1
Juan_Emilio_Garcia <theprof301@gmail.com>
2022-01-21 08:53:01 -0300

    Lazy Loading agregado

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
+import React from "react";
 import ProtectedRouter from "./helpers/ProtectedRouter/ProtectedRouter";
-import Login from "./routes/Login/Login";
-import Signup from "./routes/Singup/Signup";
-import Home from "./routes/Home/Home";
-import Container from "./routes/groups/Container";
+import Loading from "./components/Loading/LoadingPage";
 import { useApi } from "./context/ApiContext";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+
+const Login = React.lazy(() => import("./routes/Login/Login"));
+const Signup = React.lazy(() => import("./routes/Singup/Signup"));
+const Home = React.lazy(() => import("./routes/Home/Home"));
+const Container = React.lazy(() => import("./routes/groups/Container"));
 
 function App() {
   const { user } = useApi();
 
   return (
-    <Router>
-      <Switch>
-        <ProtectedRouter
-          path="/"
-          exact
-          Component={Home}
-          isAuth={user}
-          routeRedirect="/login"
-        />
-        <ProtectedRouter
-          path="/groups"
-          Component={Container}
-          isAuth={user}
-          routeRedirect="/login"
-        />
-        <ProtectedRouter
-          path="/login"
-          Component={Login}
-          isAuth={!user}
-          routeRedirect="/"
-        />
-        <ProtectedRouter
-          path="/signup"
-          Component={Signup}
-          isAuth={!user}
-          routeRedirect="/"
-        />
-      </Switch>
-    </Router>
+    <React.Suspense fallback={<Loading />}>
+      <Router>
+        <Switch>
+          <ProtectedRouter
+            path="/"
+            exact
+            Component={Home}
+            isAuth={user}
+            routeRedirect="/login"
+          />
+          <ProtectedRouter
+            path="/groups"
+            Component={Container}
+            isAuth={user}
+            routeRedirect="/login"
+          />
+          <ProtectedRouter
+            path="/login"
+            Component={Login}
+            isAuth={!user}
+            routeRedirect="/"
+          />
+          <ProtectedRouter
+            path="/signup"
+            Component={Signup}
+            isAuth={!user}
+            routeRedirect="/"
+          />
+        </Switch>
+      </Router>
+    </React.Suspense>
   );
 }
 
